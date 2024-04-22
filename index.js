a8e67565a1fd863bbbfdc00dd26a7b1fe58cba40
Lucas bezerra <140905557+lucasbezerrapro@users.noreply.github.com>
2024-04-22 20:56:21 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const app = express();
 const router = require('./passaros.js');
 
 app.use('/', router);
-
+/*
 const port = 3000;
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
 });
+*/
